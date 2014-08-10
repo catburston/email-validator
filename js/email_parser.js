dfc8b98f4af178555d31be1b0130831ac1be031c
catburston <cat1788@gmail.com>
2014-08-11 00:06:14 +0200

    replaces camouflaged @

--- js/email_parser.js
+++ js/email_parser.js
@@ -3,10 +3,10 @@ var illegalTextBlocks = ['-AT-', '(AT)', '*AT*'];
 function normalizeCamouflagedEmails(text) {
   var cleanText;
   for (var i = 0; i < illegalTextBlocks.length; i++) {
-    var tempText = text;
+    var tempText;
     if (text.indexOf(" " + illegalTextBlocks[i]) != -1) {
       // check for space in front of illegal string
-      tempText = text;
+      text = text;
     // } else if (text.indexOf(illegalTextBlocks[i]) > 0) {
     //   // check for illegal block
     //   cleanText = text;
@@ -14,10 +14,15 @@ function normalizeCamouflagedEmails(text) {
       // check for illegal string
       tempText = text.replace(illegalTextBlocks[i],"@");
     } else {
-      cleanText = text;
+      text = text;
     }
   }
-  cleanText = tempText;
+
+  if (tempText != undefined) {
+    cleanText = tempText;
+  } else {
+    cleanText = text;
+  }
   return cleanText;
 }
 
